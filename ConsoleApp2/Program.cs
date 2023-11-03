9b0c3a27e1548db8959fc1edb360e89d40ce5964
Sam1rrr <samiroruclu10@gmail.com>
2023-11-03 12:51:21 +0400

    as/is

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Vehicle vehicle1 = new Car();
+           Vehicle vehicle2 = new Bus();
+
+            Car car1 =vehicle1 as Car;
+            if(car1 != null ) 
+            {
+                car1.Drive();
+            }
+            else
+            {
+                Console.WriteLine("as islederek , car deyl");
+            }
+            Bus bus1 = vehicle1 as Bus;
+            if (bus1 != null)
+            {
+                bus1.Drive();
+            }
+            else
+            {
+                Console.WriteLine("as islederek , bus deyl");
+            }
+
+            //if (vehicle1 is Car )
+            //{
+            //    vehicle1.Drive();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bu car deyl");
+            //}
+            //if (vehicle2 is Bus)
+            //{
+            //    vehicle2.Drive();
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("bu bus deyl");
+            //}
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal abstract class Vehicle
+    {
+        public abstract void Drive();
+        public decimal Price { get; set; }
+    }
+}
